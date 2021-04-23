12a76571279c2f14370dc9a5c050612c941f55c6
Anderson <andersonluiscp@gmail.com>
2021-04-23 06:51:58 -0300

    Finish ContentProviderClient

--- app/src/main/java/com/example/contentproviderclient/ClientAdapter.kt
+++ app/src/main/java/com/example/contentproviderclient/ClientAdapter.kt
@@ -7,23 +7,32 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class ClientAdapter (private val mCursor: Cursor): RecyclerView.Adapter<ClientViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ClientViewHolder =
-        ClientViewHolder(LayoutInflater.from(parent.context)
-                .inflate(R.layout.client_item, parent, false)
-        )
+
+class ClientAdapter (private val mCursor: Cursor): RecyclerView.Adapter<ClientAdapter.ClientViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ClientViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.client_item, parent, false)
+        return ClientViewHolder(view)
+    }
 
     override fun getItemCount(): Int = mCursor.count
 
     override fun onBindViewHolder(holder: ClientViewHolder, position: Int) {
         mCursor.moveToPosition(position)
-        holder.clientTitle.text = mCursor.getString(mCursor.getColumnIndex("title"))
-        holder.clientDescription.text = mCursor.getString(mCursor.getColumnIndex("description"))
+        holder.bindData(mCursor)
     }
 
-}
+    class ClientViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        val clientTitle = itemView.findViewById<TextView>(R.id.tv_client_item_title)
+        val clientDescription = itemView.findViewById<TextView>(R.id.tv_client_item_description)
 
-class ClientViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
-    val clientTitle = itemView.findViewById(R.id.tv_client_item_title) as TextView
-    val clientDescription = itemView.findViewById(R.id.tv_client_item_description) as TextView
+        fun bindData(cursor: Cursor){
+            val title = cursor.getString(cursor.getColumnIndex("title"))
+            val description =cursor.getString(cursor.getColumnIndex("description"))
+
+            clientTitle.text = title
+            clientDescription.text = description
+        }
+
+    }
 }
